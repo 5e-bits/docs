bee01ab1ba6465e4a33e11502fddf7326fb74586
Christopher Ward <cdurianward@gmail.com>
2024-04-03 10:46:57 -0700

    Fix issue with require

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer).themes.github;
+const darkCodeTheme = require("prism-react-renderer).themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -37,9 +37,7 @@ const config = {
       }),
     ],
   ],
-  plugins: [
-    "./src/plugins/dynamic-tutorials-list"
-  ],
+  plugins: ["./src/plugins/dynamic-tutorials-list"],
   themeConfig:
     /** @type {import('docusaurus-preset-openapi').ThemeConfig} */
     ({
